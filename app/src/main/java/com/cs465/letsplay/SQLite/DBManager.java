996f43ca35f6c6559cf6f9fc3b488772aa84dfb5
Piyush <me.piyush89@gmail.com>
2015-05-01 00:12:28 -0500

    -- added 4 classes on Extensions. Db package now has relevant code for user, sport, friend and event. get and set available only for user. fragment_dbtest.xml created by Kevin for testing DB.

--- app/src/main/java/com/cs465/letsplay/SQLite/DBManager.java
+++
@@ -1,7 +0,0 @@
-package com.cs465.letsplay.SQLite;
-
-/**
- * Created by kevin on 4/28/2015.
- */
-public class DBManager {
-}
--- app/src/main/java/com/cs465/letsplay/fragmentClass/FragmentDBTest.java
+++ app/src/main/java/com/cs465/letsplay/fragmentClass/FragmentDBTest.java
@@ -5,16 +5,25 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.cs465.letsplay.Entities.User;
 import com.cs465.letsplay.R;
+import com.cs465.letsplay.SQLLite.DBManager;
 
 /**
  * Created by kevin on 4/29/2015.
  */
 public class FragmentDBTest extends Fragment
 {
-    TextView txtTitle ;
+    TextView txtResult ;
+    EditText editName;
+    EditText editId;
+    Button btnsumbit;
+    Button btnshow;
+    DBManager dbManager;
 
     public FragmentDBTest()
     {
@@ -25,8 +34,37 @@ public class FragmentDBTest extends Fragment
                              Bundle savedInstanceState)
     {
         View v = inflater.inflate(R.layout.fragment_dbtest, container, false);
-        txtTitle = (TextView)v.findViewById(R.id.T_title);
-        txtTitle.setText("something here");
+        txtResult = (TextView)v.findViewById(R.id.T_result);
+        editName = (EditText)v.findViewById(R.id.editText);
+        editId = (EditText)v.findViewById(R.id.editUserId);
+        btnshow = (Button)v.findViewById(R.id.T_btnshow);
+        btnsumbit =(Button)v.findViewById(R.id.T_btnSubmit);
+        dbManager = new DBManager(getActivity());
+
+
+
+        btnsumbit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String userName = editName.getText().toString();
+                String userId = editId.getText().toString();
+                dbManager.createUser(userName,userId);
+
+            }
+        });
+        btnshow.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                User user = dbManager.getUserInfo();
+                String userName = user.getUsername();
+                String userId = user.getUserId();
+                txtResult.setText("name : "+userName+" | "+"ID: "+String.valueOf(userId));
+
+            }
+        });
+
         return v;
     }
 }
