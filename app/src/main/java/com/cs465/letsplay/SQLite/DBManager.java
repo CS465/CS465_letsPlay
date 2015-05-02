07697634a9b06670352d004d96788820f7c40f3f
Piyush <me.piyush89@gmail.com>
2015-05-02 02:25:48 -0500

    UpdateUser method added. This is synchronous to the Edit Profile page.

--- app/src/main/java/com/cs465/letsplay/SQLite/DBManager.java
+++ app/src/main/java/com/cs465/letsplay/SQLite/DBManager.java
@@ -142,6 +142,65 @@ public class DBManager
         }
     }
 
+    //Method: updateUser. When user edits his/her profile, call should reach here.
+    // create or update a user.
+    public void updateUser(String name, String age, String baseball, String football, String pingpong,
+                           String gender, String phone, String soccer, String tennis, String address)
+    {
+        try
+        {
+            db = dbHelper.getReadableDatabase();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        // Define a projection that specifies which columns from the database
+        String[] columns = { DatabaseContract.UserTable.COLUMN_NAME_USERNAME, DatabaseContract.UserTable.COLUMN_NAME_ADDRESS,
+                DatabaseContract.UserTable.COLUMN_NAME_AGE,
+                DatabaseContract.UserTable.COLUMN_NAME_BASEBALL,
+                DatabaseContract.UserTable.COLUMN_NAME_FOOTBALL,
+                DatabaseContract.UserTable.COLUMN_NAME_GENDER,
+                DatabaseContract.UserTable.COLUMN_NAME_PHONE_NUM,
+                DatabaseContract.UserTable.COLUMN_NAME_PINGPONG,
+                DatabaseContract.UserTable.COLUMN_NAME_SOCCER,
+                DatabaseContract.UserTable.COLUMN_NAME_TENNIS,};
+
+        Cursor c = queryDatabase(DatabaseContract.UserTable.TABLE_NAME, columns, null, null,
+                null, null, null);
+        // check for the existence, if exists, update the user name
+        if( c != null && c.moveToFirst() )
+        {
+            db = dbHelper.getWritableDatabase();
+            Log.i("update User -->"," update");
+            ContentValues values = new ContentValues();
+
+            // Commit only the changed values. Check for null strings too.
+            if(name!=null)
+                values.put(DatabaseContract.UserTable.COLUMN_NAME_USERNAME, name);
+            if(address!=null)
+                values.put(DatabaseContract.UserTable.COLUMN_NAME_ADDRESS, address);
+            if(age!=null)
+                values.put(DatabaseContract.UserTable.COLUMN_NAME_AGE, age);
+            if(gender!=null)
+                values.put(DatabaseContract.UserTable.COLUMN_NAME_GENDER, gender);
+            if(tennis!=null)
+                values.put(DatabaseContract.UserTable.COLUMN_NAME_TENNIS, tennis);
+            if(football!=null)
+                values.put(DatabaseContract.UserTable.COLUMN_NAME_FOOTBALL, football);
+            if(baseball!=null)
+                values.put(DatabaseContract.UserTable.COLUMN_NAME_BASEBALL, baseball);
+            if(pingpong!=null)
+                values.put(DatabaseContract.UserTable.COLUMN_NAME_USERNAME, pingpong);
+            if(phone!=null)
+                values.put(DatabaseContract.UserTable.COLUMN_NAME_PHONE_NUM, phone);
+
+            db.update(DatabaseContract.UserTable.TABLE_NAME, values, null, null);
+            db.close();
+        }
+    }
+
     private Cursor queryDatabase(String tableName, String[] columns,
                                  String selection, String[] selectionArgs, String groupBy,
                                  String having, String orderBy)
